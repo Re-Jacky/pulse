1a8a203b67302f3ef66bc28e47e112eb26475915
zyao_mstr <zyao@strategy.com>
2026-05-22 11:13:41 +0800

    fix: guard popover toggle, remove IUOs in AppDelegate

--- mac-monitor/App/AppDelegate.swift
+++ mac-monitor/App/AppDelegate.swift
@@ -1,17 +1,17 @@
 import AppKit
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItem: NSStatusItem!
-    private var popover: NSPopover!
+    private var statusItem: NSStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    private let popover: NSPopover = {
+        let p = NSPopover()
+        p.behavior = .transient
+        p.animates = true
+        return p
+    }()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
 
-        popover = NSPopover()
-        popover.behavior = .transient
-        popover.animates = true
-
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         if let button = statusItem.button {
             button.image = NSImage(systemSymbolName: "cpu", accessibilityDescription: "System Monitor")
             button.image?.isTemplate = true
@@ -21,7 +21,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @objc private func togglePopover() {
-        guard let button = statusItem.button else { return }
+        guard let button = statusItem.button, popover.contentViewController != nil else { return }
         if popover.isShown {
             popover.performClose(nil)
         } else {
