d16b8c067b9b97e079c58ce32e436b322d5096a6
zyao_mstr <zyao@strategy.com>
2026-05-22 11:11:14 +0800

    feat: create Xcode project with AppKit lifecycle

---
+++ mac-monitor/App/AppDelegate.swift
@@ -0,0 +1,31 @@
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+
+        popover = NSPopover()
+        popover.behavior = .transient
+        popover.animates = true
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "cpu", accessibilityDescription: "System Monitor")
+            button.image?.isTemplate = true
+            button.action = #selector(togglePopover)
+            button.target = self
+        }
+    }
+
+    @objc private func togglePopover() {
+        guard let button = statusItem.button else { return }
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        }
+    }
+}
